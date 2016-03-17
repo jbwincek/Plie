6a222f4fab89c016e9d9c33d9463aba891fbac0a
Jessie Wincek <jbwincek@gmail.com>
2016-03-16 21:56:50 -0400

    ADDED: CellSpace class
    ADDED: CellSpace class which is a thin wrapper around dict to support
    2D slicing when the keys are (x,y) coordinate pairs.
    ADDED: tests for CellSpace

---
+++ plie/cellspace.py
@@ -0,0 +1,60 @@
+from collections import UserDict
+from numbers import Integral
+
+
+class CellSpace(UserDict):
+    """ CellSpace is a thin wrapper around dict, for dicts of (x,y) coordinate pairs to support 2D
+    slicing, and knowing bounds information of the CellSpace.
+
+    Notes: CellSpace does not support assigning by slice at this point.
+           Height and width are not absolute measures of how many cells CellSpace contains in
+           either direction. Instead they show the max labeled cell, this can be considered
+           either a bug or a feature. Max labeled cell in this case means the cell with the
+           largest index in that direction (height or width).
+    """
+
+    @property
+    def width(self):
+        # these are plus one because of the 0th cell being counted
+        return max(list(self.data.keys()), key=lambda t: t[0])[0] + 1
+
+    @property
+    def height(self):
+        # these are plus one because of the 0th cell being counted
+        return max(list(self.data.keys()), key=lambda t: t[1])[1] + 1
+
+    def __getitem__(self, index):
+        cls = type(self)
+        new_cells = cls()
+        width = self.width
+        height = self.height
+        if isinstance(index, tuple) and isinstance(index[0], slice) and isinstance(index[1], slice):
+            # handle the case where both items are slices aka box case
+            x_s = index[0].indices(width)
+            y_s = index[1].indices(height)
+            for y_index, y in enumerate(range(*y_s)):
+                for x_index, x in enumerate(range(*x_s)):
+                    new_cells[(x_index, y_index)] = self.data[(x, y)]
+            return new_cells
+
+        elif isinstance(index, tuple) and isinstance(index[0], slice) and isinstance(index[1], Integral):
+            # handle the case where x is a slice, but y is an integer [int:int, int] aka row case
+            x_s = index[0].indices(width)
+            for x_index, x in enumerate(range(*x_s)):
+                new_cells[(x_index, 0)] = self.data[(x, index[1])]
+            return new_cells
+
+        elif isinstance(index, tuple) and isinstance(index[0], Integral) and isinstance(index[1], slice):
+            # handle the case where x is an integer, but y is a slice [int, int:int] aka column case
+            y_s = index[1].indices(height)
+            for y_index, y in enumerate(range(*y_s)):
+                new_cells[(0, y_index)] = self.data[(index[0], y)]
+            return new_cells
+
+        elif isinstance(index, tuple) and isinstance(index[0], Integral) and isinstance(index[1], Integral):
+            # handle the case where both items are integers [int,int] aka cell case
+            return self.data[(index[0], index[1])]
+
+        else:
+            msg = '{cls.__name__} indices must be integers'
+            raise TypeError(msg.format(cls=cls))
